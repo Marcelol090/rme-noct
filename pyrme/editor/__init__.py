99cb4ca449e7122a65a3a50223347424c64cf508
Marcelol090 <rengarmarcelo.mgh@gmail.com>
2026-04-18 21:57:13 -0300

    fix(M003/S01): include editor model package

---
+++ pyrme/editor/__init__.py
@@ -0,0 +1,10 @@
+"""Editor-domain models for the Python UI shell."""
+
+from pyrme.editor.model import EditorModel, MapModel, MapPosition, TileState
+
+__all__ = [
+    "EditorModel",
+    "MapModel",
+    "MapPosition",
+    "TileState",
+]
